887da322f7100e4ce6c88d7e7a4259210725c169
joaquin Ledo <joaquin-ledo@hotmail.com>
2024-05-13 09:49:36 -0300

    Agregar archivos de proyecto.

---
+++ frmVehiculos.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace PryLedoEtapa4
+{
+    partial class frmVehiculos
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnVehiculos = new System.Windows.Forms.Button();
+            this.lstMostrarVehiculos = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // BtnVehiculos
+            // 
+            this.BtnVehiculos.BackColor = System.Drawing.Color.LightBlue;
+            this.BtnVehiculos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnVehiculos.Location = new System.Drawing.Point(265, 451);
+            this.BtnVehiculos.Name = "BtnVehiculos";
+            this.BtnVehiculos.Size = new System.Drawing.Size(441, 52);
+            this.BtnVehiculos.TabIndex = 0;
+            this.BtnVehiculos.Text = "Listar Vehiculos";
+            this.BtnVehiculos.UseVisualStyleBackColor = false;
+            this.BtnVehiculos.Click += new System.EventHandler(this.BtnVehiculos_Click);
+            // 
+            // lstMostrarVehiculos
+            // 
+            this.lstMostrarVehiculos.FormattingEnabled = true;
+            this.lstMostrarVehiculos.Location = new System.Drawing.Point(265, 350);
+            this.lstMostrarVehiculos.Name = "lstMostrarVehiculos";
+            this.lstMostrarVehiculos.Size = new System.Drawing.Size(441, 95);
+            this.lstMostrarVehiculos.TabIndex = 1;
+            // 
+            // frmVehiculos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(954, 515);
+            this.Controls.Add(this.lstMostrarVehiculos);
+            this.Controls.Add(this.BtnVehiculos);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmVehiculos";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Generacion de Vehiculos en una Lista ";
+            this.Load += new System.EventHandler(this.frmVehiculos_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmVehiculos_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnVehiculos;
+        private System.Windows.Forms.ListBox lstMostrarVehiculos;
+    }
+}
+
---
+++ frmVehiculos.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PryLedoEtapa4
+{
+    public partial class frmVehiculos : Form
+    {
+        public frmVehiculos()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+            this.KeyDown += new KeyEventHandler(frmVehiculos_KeyDown);
+            this.Focus();
+
+            objAuto = new clsVehiculos();
+            objAvion = new clsVehiculos();
+            objBarco = new clsVehiculos();
+
+            objAuto.CrearAuto();
+            objAvion.CrearAvion();
+            objBarco.CrearBarco();
+        }
+        List<clsVehiculos> listaVehiculos = new List<clsVehiculos>();
+        clsVehiculos objAuto;
+        clsVehiculos objAvion;
+        clsVehiculos objBarco;
+        
+        private void BtnVehiculos_Click(object sender, EventArgs e)
+        {
+            listaVehiculos.Add(objAuto);
+            listaVehiculos.Add(objAvion);
+            listaVehiculos.Add(objBarco);
+
+            lstMostrarVehiculos.Items.Clear();
+
+            foreach (clsVehiculos vehiculo in listaVehiculos)
+            {
+                lstMostrarVehiculos.Items.Add(vehiculo.tipoVehiculo);
+            }
+        }
+
+        private void frmVehiculos_Load(object sender, EventArgs e)
+        {
+            objAuto = new clsVehiculos();
+            objAvion = new clsVehiculos();
+            objBarco = new clsVehiculos();
+
+            objAuto.CrearAuto();
+            objAvion.CrearAvion();
+            objBarco.CrearBarco();
+        }
+
+        private void frmVehiculos_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Determinar qué tecla se presionó y llamar a la función CrearVehiculo con el tipo correspondiente
+            switch (e.KeyCode)
+            {
+                case Keys.A:
+                    CrearVehiculo("auto");
+                    break;
+                case Keys.S:
+                    CrearVehiculo("avion");
+                    break;
+                case Keys.D:
+                    CrearVehiculo("barco");
+                    break;
+            }
+        }
+        private void CrearVehiculo(string tipoVehiculo)
+        {
+            // Lógica para determinar la posición en la pantalla (aquí se muestra un ejemplo simple)
+            Random rnd = new Random();
+            int x = rnd.Next(this.Width - 100); // Ancho del formulario
+            int y = rnd.Next(this.Height - 100); // Alto del formulario 
+
+            // Crear un nuevo PictureBox para mostrar el vehículo
+            PictureBox pictureBox = new PictureBox();
+            pictureBox.Location = new Point(x, y);
+            pictureBox.Size = new Size(100, 100); // Tamaño de la imagen
+            pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            // Crear un nuevo Label para mostrar el tipo de vehículo
+            Label label = new Label();
+            label.Text = tipoVehiculo;
+            label.AutoSize = true;
+            label.Location = new Point(x, y + 100); // Colocar el Label debajo del PictureBox
+
+            // Crear un nuevo vehículo
+            switch (tipoVehiculo)
+            {
+                case "auto":
+                    pictureBox.Image = (Image)Properties.Resources.ResourceManager.GetObject("auto");
+                    break;
+                case "barco":
+                    pictureBox.Image = (Image)Properties.Resources.ResourceManager.GetObject("barco");
+                    break;
+                case "avion":
+                    pictureBox.Image = (Image)Properties.Resources.ResourceManager.GetObject("avion");
+                    break;
+                default:
+                    return; // No se reconoce el tipo de vehículo, salir sin crear nada
+            }
+
+            // Agregar el PictureBox y el Label al formulario
+            this.Controls.Add(pictureBox);
+            this.Controls.Add(label);
+
+        }
+    }
+}
